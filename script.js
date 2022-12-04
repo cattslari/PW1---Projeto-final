31077361c723aba908aa84ff726c187e667ef1f9
cattslari <114114544+cattslari@users.noreply.github.com>
2022-12-03 23:02:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var usuario = document.getElementById('usuario')
+var btn = document.getElementById('btn')
+var demo = document.getElementById('demo')
+
+function verifica() {
+    demo.innerHTML = usuario.value
+}
